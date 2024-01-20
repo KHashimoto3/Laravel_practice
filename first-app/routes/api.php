77a9e766b8cbe9a32af6a025e9da9916df7a4d26
KHashimoto3 <hashi.koki385@gmail.com>
2024-01-20 17:09:46 +0900

    hello world apiを作成した。

--- first-app/routes/api.php
+++ first-app/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HelloController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('api')->group(function () {
+    Route::get('/hello', 'App\Http\Controllers\HelloController@hello');
 });
